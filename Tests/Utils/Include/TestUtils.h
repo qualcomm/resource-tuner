d3f568cedcc1a49ee77f5d1360c2d0196121f8d3
kartnema <kartnema@qti.qualcomm.com>
2025-10-01 10:55:16 +0530

    Modify node extensions (#32)
    ---
    Signed-off-by: kartnema <kartnema@qti.qualcomm.com>

--- Tests/Utils/Include/TestUtils.h
+++ Tests/Utils/Include/TestUtils.h
@@ -37,15 +37,15 @@
 void SetUp() {
     // Make sure all the tests have a sane starting point
 
-    AuxRoutines::writeToFile("/etc/resource-tuner/tests/Configs/ResourceSysFsNodes/sched_util_clamp_min", "300");
-    AuxRoutines::writeToFile("/etc/resource-tuner/tests/Configs/ResourceSysFsNodes/sched_util_clamp_max", "684");
-    AuxRoutines::writeToFile("/etc/resource-tuner/tests/Configs/ResourceSysFsNodes/scaling_min_freq", "107");
-    AuxRoutines::writeToFile("/etc/resource-tuner/tests/Configs/ResourceSysFsNodes/scaling_max_freq", "114");
-    AuxRoutines::writeToFile("/etc/resource-tuner/tests/Configs/ResourceSysFsNodes/target_test_resource1", "240");
-    AuxRoutines::writeToFile("/etc/resource-tuner/tests/Configs/ResourceSysFsNodes/target_test_resource2", "333");
-    AuxRoutines::writeToFile("/etc/resource-tuner/tests/Configs/ResourceSysFsNodes/target_test_resource3", "4400");
-    AuxRoutines::writeToFile("/etc/resource-tuner/tests/Configs/ResourceSysFsNodes/target_test_resource4", "516");
-    AuxRoutines::writeToFile("/etc/resource-tuner/tests/Configs/ResourceSysFsNodes/target_test_resource5", "17");
+    AuxRoutines::writeToFile("/etc/resource-tuner/tests/Configs/ResourceSysFsNodes/sched_util_clamp_min.txt", "300");
+    AuxRoutines::writeToFile("/etc/resource-tuner/tests/Configs/ResourceSysFsNodes/sched_util_clamp_max.txt", "684");
+    AuxRoutines::writeToFile("/etc/resource-tuner/tests/Configs/ResourceSysFsNodes/scaling_min_freq.txt", "107");
+    AuxRoutines::writeToFile("/etc/resource-tuner/tests/Configs/ResourceSysFsNodes/scaling_max_freq.txt", "114");
+    AuxRoutines::writeToFile("/etc/resource-tuner/tests/Configs/ResourceSysFsNodes/target_test_resource1.txt", "240");
+    AuxRoutines::writeToFile("/etc/resource-tuner/tests/Configs/ResourceSysFsNodes/target_test_resource2.txt", "333");
+    AuxRoutines::writeToFile("/etc/resource-tuner/tests/Configs/ResourceSysFsNodes/target_test_resource3.txt", "4400");
+    AuxRoutines::writeToFile("/etc/resource-tuner/tests/Configs/ResourceSysFsNodes/target_test_resource4.txt", "516");
+    AuxRoutines::writeToFile("/etc/resource-tuner/tests/Configs/ResourceSysFsNodes/target_test_resource5.txt", "17");
 
     std::this_thread::sleep_for(std::chrono::seconds(3));
 }
