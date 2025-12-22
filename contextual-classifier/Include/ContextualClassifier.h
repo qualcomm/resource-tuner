d6d559bf824ef57888a58bf6a3311dfa488b537e
Jagadeesh Pagadala <jpagadal@qti.qualcomm.com>
2025-12-22 17:24:58 +0530

    Code re-org, Classifier Extension Module (#84)
    * Add Extension handling from classifier
    * Move from Queue + set implementation to Queue for PID event Q
    * Move Netlink handling from Classifier to NetLinkComm
    * Convert Prediction string to an int
    * clang-format - format the code
    
    Signed-off-by: Jagadeesh Pagadala <jpagadal@qti.qualcomm.com>

--- contextual-classifier/Include/ContextualClassifier.h
+++ contextual-classifier/Include/ContextualClassifier.h
@@ -16,6 +16,30 @@
 #include <unordered_set>
 #include <vector>
 
+enum { CC_IGNORE = 0x00, CC_APP_OPEN = 0x01, CC_APP_CLOSE = 0x02 };
+
+enum {
+    CC_BROWSER_APP_OPEN = 0x00,
+    CC_GAME_APP_OPEN = 0x01,
+    CC_MULTIMEDIA_APP_OPEN = 0x02
+};
+
+enum { DEFAULT_CONFIG, PER_APP_CONFIG };
+
+typedef enum CC_TYPE {
+    CC_APP = 0x01,
+    CC_BROWSER = 0x02,
+    CC_GAME = 0x03,
+    CC_MULTIMEDIA = 0x04,
+    CC_UNKNOWN = 0x05
+} CC_TYPE;
+
+struct ProcEvent {
+    int pid;
+    int tgid;
+    int type; // CC_APP_OPEN / CC_APP_CLOSE / CC_IGNORE
+};
+
 class ContextualClassifier {
   public:
     ContextualClassifier();
@@ -24,23 +48,23 @@ class ContextualClassifier {
     ErrCode Terminate();
 
   private:
-    void WorkerThread();
+    void ClassifierMain();
     int HandleProcEv();
-    void ClassifyProcess(int pid, int tgid,
-                         std::unordered_map<int, int> &pid_perf_handle);
+    int ClassifyProcess(int pid, int tgid, const std::string &comm,
+                        uint32_t &ctxDetails);
     void LoadIgnoredProcesses();
-    void RemoveActions(int pid, int tgid,
-                       std::unordered_map<int, int> &pid_perf_handle);
+    void ApplyActions(std::string comm, int32_t sigId, int32_t sigType);
+    void RemoveActions(int pid, int tgid);
+    bool isIgnoredProcess(int evType, int pid);
 
     NetLinkComm mNetLinkComm;
     MLInference mMLInference;
 
-    // FIFO queue + pending set for PID lifecycle management
-    std::queue<int> mClassificationQueue; // FIFO of PIDs
-    std::unordered_set<int> mPendingPids; // PIDs still pending/alive
+    // Event queue for classifier main thread
+    std::queue<ProcEvent> mPendingEv;
     std::mutex mQueueMutex;
     std::condition_variable mQueueCond;
-    std::thread mWorkerThread;
+    std::thread mClassifierMain;
     std::thread mNetlinkThread;
     volatile bool mNeedExit = false;
 
@@ -48,6 +72,12 @@ class ContextualClassifier {
     std::unordered_map<std::string, std::unordered_set<std::string>>
         mTokenIgnoreMap;
     bool mDebugMode = false;
+
+    std::unordered_set<int> mIgnoredPids;
+    std::unordered_map<int, uint64_t> mResTunerHandles;
+
+    void GetSignalDetailsForWorkload(int32_t contextType, uint32_t &sigId,
+                                     uint32_t &sigSubtype);
 };
 
 #endif // CONTEXTUAL_CLASSIFIER_H
